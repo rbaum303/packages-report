ea9973aecf2256ede592d03f3b5a603050047af3
Rachel Baum <rachelbaum@50-204-120-225-static.hfc.comcastbusiness.net>
2020-01-27 16:07:21 -0800

    Add R script, .R proj file, Report

---
+++ testPackagesReport.R
@@ -0,0 +1,24 @@
+packages <- installed.packages()
+
+## if you use dplyr, code like this will work:
+apt <- packages %>%
+  filter(is.na(Priority)) %>%
+  select(Package, Built)
+
+## write this new, smaller data frame to data/add-on-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+## make a frequency table of package by the version in Built
+## if you use dplyr, code like this will work:
+apt_freqtable <- packages %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
+## write this data frame to data/add-on-packages-freqtable.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+## YES overwrite the files that are there now
+## they are old output from me (Jenny)
+## they are just examples
